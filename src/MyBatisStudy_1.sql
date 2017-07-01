6f1eb8d16301439acf439ab7f1a55d8c4ecd7a22
ChuanMac <845825101@qq.com>
2017-07-01 16:58:24 +0800

    MyBatis CRUD

---
+++ src/MyBatisStudy_1.sql
@@ -0,0 +1,5 @@
+create database mybatis;
+2 use mybatis;
+3 CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), age INT);
+4 INSERT INTO users(name, age) VALUES('cherish', 21);
+5 INSERT INTO users(name, age) VALUES('christine', 20);
